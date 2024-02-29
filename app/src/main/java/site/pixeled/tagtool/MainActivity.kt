0c350c457181069a0621f5c8928375ea27d5ca9a
orosmatthew <orosmatthew@pm.me>
2024-02-29 14:22:57 -0500

    Add fragments

--- app/src/main/java/site/pixeled/tagtool/MainActivity.kt
+++ app/src/main/java/site/pixeled/tagtool/MainActivity.kt
@@ -15,31 +15,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val scannerView = findViewById<CodeScannerView>(R.id.scannerView)
-        mCodeScanner = CodeScanner(this, scannerView)
-
-        mCodeScanner.camera = CodeScanner.CAMERA_BACK
-        mCodeScanner.formats = CodeScanner.ALL_FORMATS
-        mCodeScanner.autoFocusMode = AutoFocusMode.SAFE
-        mCodeScanner.scanMode = ScanMode.SINGLE
-        mCodeScanner.isAutoFocusEnabled = true
-        mCodeScanner.isFlashEnabled = false
-
-        mCodeScanner.setDecodeCallback {
-            runOnUiThread {
-                Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
-                mCodeScanner.startPreview();
-            }
-        }
+//        val scannerView = findViewById<CodeScannerView>(R.id.scannerView)
+//        mCodeScanner = CodeScanner(this, scannerView)
+//
+//        mCodeScanner.camera = CodeScanner.CAMERA_BACK
+//        mCodeScanner.formats = CodeScanner.ALL_FORMATS
+//        mCodeScanner.autoFocusMode = AutoFocusMode.SAFE
+//        mCodeScanner.scanMode = ScanMode.SINGLE
+//        mCodeScanner.isAutoFocusEnabled = true
+//        mCodeScanner.isFlashEnabled = false
+//
+//        mCodeScanner.setDecodeCallback {
+//            runOnUiThread {
+//                Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
+//                mCodeScanner.startPreview();
+//            }
+//        }
     }
 
-    override fun onResume() {
-        super.onResume()
-        mCodeScanner.startPreview()
-    }
-
-    override fun onPause() {
-        mCodeScanner.releaseResources()
-        super.onPause()
-    }
+//    override fun onResume() {
+//        super.onResume()
+//        mCodeScanner.startPreview()
+//    }
+//
+//    override fun onPause() {
+//        mCodeScanner.releaseResources()
+//        super.onPause()
+//    }
 }
