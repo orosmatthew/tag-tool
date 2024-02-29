1e1376459235efc6f2bbb4ffb1eda191a0c407ea
orosmatthew <orosmatthew@pm.me>
2024-02-29 13:53:58 -0500

    Add code scanner

--- app/src/main/java/site/pixeled/tagtool/MainActivity.kt
+++ app/src/main/java/site/pixeled/tagtool/MainActivity.kt
@@ -2,10 +2,44 @@ package site.pixeled.tagtool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.ScanMode
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mCodeScanner: CodeScanner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val scannerView = findViewById<CodeScannerView>(R.id.scannerView)
+        mCodeScanner = CodeScanner(this, scannerView)
+
+        mCodeScanner.camera = CodeScanner.CAMERA_BACK
+        mCodeScanner.formats = CodeScanner.ALL_FORMATS
+        mCodeScanner.autoFocusMode = AutoFocusMode.SAFE
+        mCodeScanner.scanMode = ScanMode.SINGLE
+        mCodeScanner.isAutoFocusEnabled = true
+        mCodeScanner.isFlashEnabled = false
+
+        mCodeScanner.setDecodeCallback {
+            runOnUiThread {
+                Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
+                mCodeScanner.startPreview();
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mCodeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        mCodeScanner.releaseResources()
+        super.onPause()
     }
 }
