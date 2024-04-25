18f9912c64ea63869a7b1f4c970f666511d864f8
orosmatthew <orosmatthew@pm.me>
2024-04-25 15:46:04 -0400

    Add userId from user auth

--- services/tag-tool/controllers/Request.php
+++ services/tag-tool/controllers/Request.php
@@ -6,10 +6,12 @@ class Request
 {
     public ?object $data;
     public ?int $id;
+    public ?int $userId;
 
-    public function __construct($data = null, int $id = null)
+    public function __construct($data = null, int $id = null, int $userId = null)
     {
         $this->data = $data;
         $this->id = $id;
+        $this->userId = $userId;
     }
 }
