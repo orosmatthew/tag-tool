f244bad99a84289726bb87479450d11c63a43961
jverhosek <jt3enigma@gmail.com>
2024-04-29 10:33:29 -0400

    Error Nav
    Error page navigation built

--- app/app/src/main/java/site/pixeled/tagtool/ErrorNotFoundFragment.kt
+++ app/app/src/main/java/site/pixeled/tagtool/ErrorNotFoundFragment.kt
@@ -5,14 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
+import androidx.navigation.Navigation
 
 class ErrorNotFoundFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_error_not_found, container, false)
+        val view = inflater.inflate(R.layout.fragment_error_not_found, container, false)
+        view.findViewById<Button>(R.id.toEaster).setOnClickListener {
+            Navigation.findNavController(view)
+                .navigate(R.id.action_errorNotFoundFragment_to_easterEggFragment)
+        }
+        view.findViewById<Button>(R.id.AddItemError).setOnClickListener {
+            Navigation.findNavController(view)
+                .navigate(R.id.action_errorNotFoundFragment_to_addItemFragment)
+        }
+        view.findViewById<Button>(R.id.ScanAgainError).setOnClickListener {
+            Navigation.findNavController(view)
+                .navigate(R.id.action_errorNotFoundFragment_to_scannerFragment)
+        }
+        view.findViewById<Button>(R.id.HomeFromError).setOnClickListener {
+            Navigation.findNavController(view)
+                .navigate(R.id.action_errorNotFoundFragment_to_welcomeUserFragment)
+        }
+        return view
     }
 
 }
