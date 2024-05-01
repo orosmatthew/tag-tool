675b682fdf2baac85d1df38f59646975d67c375d
YummyDio <azink20@bw.edu>
2024-05-01 11:16:53 -0400

    Button Navigation for easter

--- app/app/src/main/java/site/pixeled/tagtool/EasterEggFragment.kt
+++ app/app/src/main/java/site/pixeled/tagtool/EasterEggFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.navigation.Navigation
 
 
@@ -19,6 +20,10 @@ class EasterEggFragment : Fragment() {
         view.findViewById<Button>(R.id.EasterBackOne).setOnClickListener {
             Navigation.findNavController(view)
                 .navigate(R.id.action_easterEggFragment_to_errorNotFoundFragment)
+        }
+        view.findViewById<ImageButton>(R.id.Emobutton).setOnClickListener {
+            Navigation.findNavController(view)
+                .navigate(R.id.action_easterEggFragment_to_baldFragment)
         }
         return view
     }
